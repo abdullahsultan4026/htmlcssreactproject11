f49c5e757c2c096ca68c75521cd630917b75fb6b
abdullahsultan4026 <abdullahsultan4026@gmail.com>
2026-03-31 02:38:27 -0700

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <>
+      <div className="card">
+        <img src="money.png" id="para1" alt="card logo" />
+
+        <h1 id="para2">Master Card</h1>
+
+        <img src="sim.png" id="para3" alt="sim chip" />
+
+        <h1 id="para4">Card Number</h1>
+        <h1 id="para5">8050 5040 2030 3020</h1>
+
+        <h1 id="para6">Prem Kumar Shahi</h1>
+
+        <h1 id="para7">Valid Thru</h1>
+      </div>
+    </>
+  );
+}
